9dde82f0d6ae84bcbb6dc023f9e724a1e683e3f0
dandanloveJM <gigi834921748@qq.com>
2020-07-05 23:31:11 +0800

    创å创建三个页面并设置导航

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div>
+<!--    展示不同的router-->
+    <router-view></router-view>
+    <hr>
+    <div>
+      <router-link to="/money">记账</router-link>
+      <router-link to="/labels">标签</router-link>
+      <router-link to="/statistics">统计</router-link>
     </div>
-    <router-view/>
   </div>
 </template>
 
---
+++ src/views/Labels.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>labels</div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Labels'
+  };
+</script>
+
+<style scoped lang="scss">
+
+</style>
