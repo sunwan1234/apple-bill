21b0a11c37845ac3a42a9a312a8ecdd55bccd61b
dandanloveJM <gigi834921748@qq.com>
2020-07-06 16:14:30 +0800

    Money页面底部Nav布局

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,37 @@
 <template>
-  <div>
-<!--    展示不同的router-->
-    <router-view></router-view>
-    <hr>
+	<div>
+		<!--    展示不同的router-->
+		<router-view></router-view>
+		<hr>
 
-  </div>
+	</div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
 
-#nav {
-  padding: 30px;
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+	}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+	#nav {
+		padding: 30px;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+		a {
+			font-weight: bold;
+			color: #2c3e50;
+
+			&.router-link-exact-active {
+				color: #42b983;
+			}
+		}
+	}
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,9 @@
 <template>
-	<div>
-		Money
+	<div class="nav-wrapper">
+		<div class="content">
+			Money
+		</div>
+
 		<Nav></Nav>
 	</div>
 
@@ -13,5 +16,16 @@
 </script>
 
 <style scoped lang="scss">
+	.nav-wrapper {
+		display: flex;
+		border: 1px solid green;
+		flex-direction: column;
+		height: 100vh;
+	}
 
+	.content {
+		flex-grow: 1; // 让内容尽量占满
+		overflow: auto;
+		border: 1px solid blue;
+	}
 </style>
