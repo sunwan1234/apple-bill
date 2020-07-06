5b2bd020910161546bd035d96c504f3e8b0dc140
dandanloveJM <gigi834921748@qq.com>
2020-07-06 17:42:39 +0800

    创建icon组件

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,13 @@ import router from './router'
 import store from './store'
 import Nav from '@/views/Nav.vue';
 import Layout from '@/components/Layout.vue';
+import Icon from '@/components/Icon.vue';
 
 Vue.config.productionTip = false
 // 全局引入nav
 Vue.component('Nav', Nav)
 Vue.component('Layout', Layout)
-
+Vue.component('Icon', Icon)
 new Vue({
   router:router,
   store,
